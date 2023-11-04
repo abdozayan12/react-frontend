8326969cbb9612823c4f922985fe1220e1a9f012
ABDEL HAKEM ZAYAN <abdozayan12@gmail.com>
2023-11-04 21:04:41 +0200

    fix linters

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchGreetings } from '../store/greetingsSlice';
+import { fetchGreetings } from './store/greetingsSlice';
 
 function Greetings() {
   const dispatch = useDispatch();
